32ddbc87901c478dab933c256b0fd05425ecdd88
MuhammadArif-cell <133563874+MuhammadArif-cell@users.noreply.github.com>
2024-01-20 20:01:08 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+ <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      School Management System
+    </div>
+    <!-- Default to the left -->
+    <strong>Fastians SE 2023.</strong> .
+  </footer>
